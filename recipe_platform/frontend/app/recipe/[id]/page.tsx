e00de53f870425d63fd0481eabc3091874c608ca
pgm-tristanderidder <tristan.deridder@student.arteveldehs.be>
2024-10-14 15:07:09 +0200

    put all dummy data on detail page

--- recipe_platform/frontend/app/recipe/[id]/page.tsx
+++ recipe_platform/frontend/app/recipe/[id]/page.tsx
@@ -13,6 +13,7 @@ export default async function RecipeDetailPage({ params }: RecipeProps) {
   const filePath = path.join(process.cwd(), "public/data/dummy.json");
   const data = await fs.readFile(filePath, "utf-8");
   const recipes: Recipe[] = JSON.parse(data).recipes;
+  const comments: Comment[] = JSON.parse(data).comments;
 
   // Find the recipe that matches the ID in the URL
   const recipe = recipes.find((recipe) => recipe.id === parseInt(params.id));
@@ -24,10 +25,53 @@ export default async function RecipeDetailPage({ params }: RecipeProps) {
 
   return (
     <div>
-      <h1>{recipe.name}</h1>
-      <p>Category: {recipe.category}</p>
-      <p>Rating: {recipe.rating}</p>
-      <RecipeItem recipe={recipe} />
+      <div>
+        <div>
+          <h1>{recipe.name}</h1>
+          <p>{recipe.category}</p>
+        </div>
+        <div>
+          <p>{recipe.rating}</p>
+          Like
+        </div>
+      </div>
+      <div>
+        <h2>Used Equipment</h2>
+        <ul>
+          {recipe.equipment.map((equipment, index) => (
+            <li key={index}>{equipment}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Ingredients</h2>
+        <ul>
+          {recipe.ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <div>
+          <h2>Instructions</h2>
+          <ol>
+            {recipe.instructions.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
+        </div>
+        <div>
+          <h2>Comments</h2>
+          {recipe.comments.map((comment, index) => (
+            <div key={index}>
+              <p>
+                <strong>{comment.user}:</strong>
+              </p>
+              <p>{comment.message}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- recipe_platform/frontend/app/types/Recipe.ts
+++ recipe_platform/frontend/app/types/Recipe.ts
@@ -1,8 +1,17 @@
 export type Recipe = {
-    id: number;
-    name: string;
-    ingredients: string[];
-    instructions: string[];
-    rating: number;
-    category: string;
-}
+  id: number;
+  name: string;
+  ingredients: string[];
+  instructions: string[];
+  rating: number;
+  category: string;
+  equipment: string[];
+  comments: Comment[];
+};
+
+
+export type Comment = {
+  user: string;
+  message: string;
+};
+
